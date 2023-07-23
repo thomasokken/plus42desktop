ae21dbf108a7b24ac43a1a16634f296eac904f04
Thomas Okken <thomasokken@gmail.com>
2023-07-24 00:06:41 +0200

    Another ANUM fix

--- common/core_helpers.cc
+++ common/core_helpers.cc
@@ -694,6 +694,8 @@ int anum(const char *text, int len, phloat *res) {
             } else if (c == '.') {
                 /* ignore */
             } else {
+                if (!have_mant_digits)
+                    goto retry;
                 break;
             }
         } else {
