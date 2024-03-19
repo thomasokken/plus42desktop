1af6207dcbc087d4ba56bf6fe143cc8d70d3494f
Thomas Okken <thomasokken@gmail.com>
2024-03-19 20:14:34 +0100

    Replacing WebView with WKWebView

--- mac/Classes/Free42AppDelegate.mm
+++ mac/Classes/Free42AppDelegate.mm
@@ -821,7 +821,8 @@ static void tbnonewliner() {
     if (!loadSkinsWindowMapped) {
         NSString *url = @"https://thomasokken.com/plus42/skins/";
         [loadSkinsURL setStringValue:url];
-        [loadSkinsWebView setMainFrameURL:url];
+        NSURLRequest *req = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+        [loadSkinsWebView loadRequest:req];
         loadSkinsWindowMapped = true;
     }
     [loadSkinsWindow makeKeyAndOrderFront:self];
@@ -830,7 +831,8 @@ static void tbnonewliner() {
 
 - (IBAction) loadSkinsGo:(id)sender {
     NSString *url = [loadSkinsURL stringValue];
-    [loadSkinsWebView setMainFrameURL:url];
+    NSURLRequest *req = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+    [loadSkinsWebView loadRequest:req];
 }
 
 - (IBAction) loadSkinsBack:(id)sender {
@@ -841,13 +843,13 @@ static void tbnonewliner() {
     [loadSkinsWebView goForward];
 }
 
-- (void)webView:(WebView *)sender didStartProvisionalLoadForFrame:(WebFrame *)frame {
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
     [loadSkinButton setEnabled:NO];
     [loadSkinButton setTitle:@"..."];
 }
 
-- (void)webView:(WebView *)sender didFinishLoadForFrame:(WebFrame *)frame {
-    NSString *url = [loadSkinsWebView mainFrameURL];
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    NSString *url = [[loadSkinsWebView URL] absoluteString];
     [loadSkinsURL setStringValue:url];
     [loadSkinButton setTitle:@"Load"];
     [loadSkinButton setEnabled:[Free42AppDelegate skinUrlPair:url] != nil];
