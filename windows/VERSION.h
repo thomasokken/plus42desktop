18f9e28a5ccb48d0f9a182d4b3fb2ee0b657e18d
Thomas Okken <thomasokken@gmail.com>
2024-03-26 23:54:21 +0100

    Release 1.1.8

--- windows/VERSION.h
+++ windows/VERSION.h
@@ -1,3 +1,3 @@
-#define PLUS42_VERSION_1 "Plus42 1.1.7"
-#define PLUS42_VERSION_2 "1.1.7\0"
-#define PLUS42_VERSION_3 1,1,7,0
+#define PLUS42_VERSION_1 "Plus42 1.1.8"
+#define PLUS42_VERSION_2 "1.1.8\0"
+#define PLUS42_VERSION_3 1,1,8,0
