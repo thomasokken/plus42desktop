2f4b21f2b399ca57c0e13a0cdc1a621c4d564599
Thomas Okken <thomasokken@gmail.com>
2024-05-28 06:46:44 +0200

    Release 1.1.12

--- windows/VERSION.h
+++ windows/VERSION.h
@@ -1,3 +1,3 @@
-#define PLUS42_VERSION_1 "Plus42 1.1.11"
-#define PLUS42_VERSION_2 "1.1.11\0"
-#define PLUS42_VERSION_3 1,1,11,0
+#define PLUS42_VERSION_1 "Plus42 1.1.12"
+#define PLUS42_VERSION_2 "1.1.12\0"
+#define PLUS42_VERSION_3 1,1,12,0
