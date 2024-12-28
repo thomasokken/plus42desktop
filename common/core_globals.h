700f9b22bf115c690225d02439b545ba51b839d5
Thomas Okken <thomasokken@gmail.com>
2024-12-28 16:47:13 +0100

    Asynchronous beeper
    This should fix the ANRs in the Android version.

--- common/core_globals.h
+++ common/core_globals.h
@@ -475,6 +475,7 @@ extern bool mode_running;
 extern bool mode_getkey;
 extern bool mode_getkey1;
 extern bool mode_pause;
+extern bool mode_tone;
 extern bool mode_disable_stack_lift;
 extern bool mode_varmenu;
 extern int mode_varmenu_whence;
--- common/core_main.cc
+++ common/core_main.cc
@@ -289,7 +289,7 @@ bool core_keydown(int key, bool *enqueued, int *repeat) {
 
     if (key == KEY_SHIFT) {
         set_shift(!mode_shift);
-        return (mode_running && !mode_getkey && !mode_pause) || keybuf_head != keybuf_tail;
+        return (mode_running && !mode_getkey && !mode_pause && !mode_tone) || keybuf_head != keybuf_tail;
     }
 
     if (mode_pause) {
@@ -301,6 +301,16 @@ bool core_keydown(int key, bool *enqueued, int *repeat) {
             redisplay();
             return 0;
         }
+    }
+
+    if (mode_tone) {
+        if (((keybuf_head + 1) & 15) != keybuf_tail) {
+            if (mode_shift)
+                key = -key;
+            keybuf[keybuf_head] = key;
+            keybuf_head = (keybuf_head + 1) & 15;
+        }
+        return 0;
     }
 
     if (mode_interruptible != NULL) {
@@ -537,6 +547,12 @@ bool core_timeout3(bool repaint) {
         }
         return true;
     }
+    if (mode_tone) {
+        mode_tone = false;
+        if (repaint && !program_running())
+            redisplay();
+        return true;
+    }
     /* Remove the output of SHOW, MEM, or shift-VARMENU from the display */
     if (pending_command == CMD_LINGER1)
         pending_command = CMD_CANCELLED;
@@ -580,7 +596,7 @@ static void print_equation_segment(int4 pc) {
 }
 
 bool core_keyup() {
-    if (mode_pause) {
+    if (mode_pause || mode_tone) {
         /* The only way this can happen is if they key in question was Shift */
         return false;
     }
@@ -761,9 +777,9 @@ bool core_keyup() {
 
     handle_error(error);
     pending_command = CMD_NONE;
-    if (!mode_getkey && !mode_pause)
+    if (!mode_getkey && !mode_pause && !mode_tone)
         redisplay();
-    return (mode_running && !mode_getkey && !mode_pause) || keybuf_head != keybuf_tail;
+    return (mode_running && !mode_getkey && !mode_pause && !mode_tone) || keybuf_head != keybuf_tail;
 }
 
 bool core_powercycle() {
@@ -5186,6 +5202,8 @@ static void continue_running() {
             shell_request_timeout3(1000);
             return;
         }
+        if (mode_tone)
+            return;
         if (error == ERR_INTERRUPTIBLE)
             return;
         if (!handle_error(error))
