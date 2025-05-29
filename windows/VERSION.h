dadb4f5d05fe228d0afe7890070375ed2e7e8fb5
Thomas Okken <thomasokken@gmail.com>
2025-05-29 17:00:37 +0200

    Release 1.3.7

--- windows/VERSION.h
+++ windows/VERSION.h
@@ -1,3 +1,3 @@
-#define PLUS42_VERSION_1 "Plus42 1.3.6"
-#define PLUS42_VERSION_2 "1.3.6\0"
-#define PLUS42_VERSION_3 1,3,6,0
+#define PLUS42_VERSION_1 "Plus42 1.3.7"
+#define PLUS42_VERSION_2 "1.3.7\0"
+#define PLUS42_VERSION_3 1,3,7,0
